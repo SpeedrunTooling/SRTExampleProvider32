61c53f2c8d18820ab25dbd4b985a69f4307ec634
Christopher D Couture <videogameroulette@yahoo.ca>
2021-07-30 08:44:55 -0400

    initial commit

---
+++ SRTExampleProvider32/GameHashes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace SRTExampleProvider32
+{
+    /// <summary>
+    /// SHA256 hashes for the RE1/BIO1 game executables.
+    /// </summary>
+    /// 
+
+    public enum GameVersion
+    {
+        UNKNOWN,
+        GameName_Region_ReleaseData_Patch
+    }
+
+    public static class GameHashes
+    {
+        // Ex Name: ResidentEvil1Classic_WorldWide_20210730_1_00
+        private static readonly byte[] GameName_Region_ReleaseData_Patch_Revision = new byte[32] { 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0 };
+
+        public static GameVersion DetectVersion(string filePath)
+        {
+            byte[] checksum;
+            using (SHA256 hashFunc = SHA256.Create())
+            using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite | FileShare.Delete))
+                checksum = hashFunc.ComputeHash(fs);
+
+            if (checksum.SequenceEqual(GameName_Region_ReleaseData_Patch_Revision))
+                return GameVersion.GameName_Region_ReleaseData_Patch;
+            else
+                return GameVersion.UNKNOWN;
+        }
+    }
+}
---
+++ SRTExampleProvider32/GameMemoryExample.cs
@@ -0,0 +1,15 @@
+﻿using SRTExampleProvider32.Structs.GameStructs;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace SRTExampleProvider32
+{
+    public class GameMemoryExample : IGameMemoryExample
+    {
+        public string GameName => "Example Game Name";
+        public string VersionInfo => FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion;
+
+        public GameExampleStruct Example { get => _example; set => _example = value; }
+        internal GameExampleStruct _example;
+    }
+}
